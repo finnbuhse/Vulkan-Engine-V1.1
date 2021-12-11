876e086c42e8dc8f287a6073e02b5cfa21c0aeed
Finn Buhse <50274957+finnbuhse@users.noreply.github.com>
2021-12-11 13:58:44 +0000

    Add files via upload

---
+++ CameraController.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "WindowManager.h"
+#include "Transform.h"
+#include "glm/glm.hpp"
+
+struct CameraController
+{
+	float movementSpeed;
+	float mouseSensitivity;
+	bool movement;
+	bool pitch;
+	bool yaw;
+};
+
+class CameraControllerSystem
+{
+private:
+	ComponentManager<Transform>& mTransformManager = ComponentManager<Transform>::instance();
+	ComponentManager<CameraController>& mCameraControllerManager = ComponentManager<CameraController>::instance();
+	WindowManager& mWindowManager = WindowManager::instance();
+
+	const ComponentAddedCallback mComponentAddedCallback = std::bind(&CameraControllerSystem::componentAdded, this, std::placeholders::_1);
+	const ComponentRemovedCallback mComponentRemovedCallback = std::bind(&CameraControllerSystem::componentRemoved, this, std::placeholders::_1);
+
+	Composition mComposition;
+	std::vector<EntityID> mEntityIDs;
+
+	glm::vec2 mLastCursorPosition;
+
+	CameraControllerSystem();
+
+public:
+	static CameraControllerSystem& instance();
+
+	CameraControllerSystem(const CameraControllerSystem& copy) = delete;
+	~CameraControllerSystem();
+
+	void componentAdded(const Entity& entity);
+	void componentRemoved(const Entity& entity);
+
+	void update(const float& deltaTime);
+};
